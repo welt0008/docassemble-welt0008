6289e58299e44ea505e33edac1b6a513d97128d7
Julia Weltner <62468155+welt0008@users.noreply.github.com>
2020-04-09 12:24:53 +0930

    commit

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.welt0008',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.welt0008\n\nA docassemble extension.\n\n## Author\n\nJulia Weltner, welt0008@flinders.edu.au\n\n',
+      long_description='# docassemble.welt0008\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nJulia Weltner, welt0008@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Julia Weltner',
       author_email='welt0008@flinders.edu.au',
